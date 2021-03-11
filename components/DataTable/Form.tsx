eaa3760d1d9387cf3f329a10781797df0ad57b10
niklas gadeberg <nikl2207@tietgen.dk>
2021-03-11 13:05:12 +0100

    added unique keys to all options

--- components/DataTable/Form.tsx
+++ components/DataTable/Form.tsx
@@ -12,7 +12,7 @@ export default function Form(props: Props) {
         // runs 10 years back
         for (let i = 0; i < 10; i++) {
             const value = new Date().getFullYear() - i;
-            element.push(<option value={value} >{value}</option>);
+            element.push(<option key={value} value={value} >{value}</option>);
         }
         return element
     })();
@@ -28,13 +28,13 @@ export default function Form(props: Props) {
 
             for (let i = 1; i <= date.getMonth() - 1; i++) {
                 const value = i.toLocaleString('en-US', { minimumIntegerDigits: 2 });
-                element.push(<option value={`m${value}`} >{i}</option>);
+                element.push(<option key={`m${value}`} value={`m${value}`} >{i}</option>);
             }
         } else if (props.year != "") {
 
             for (let i = 1; i <= 12; i++) {
                 const value = i.toLocaleString('en-US', { minimumIntegerDigits: 2 });
-                element.push(<option value={`m${value}`} >{i}</option>);
+                element.push(<option key={`m${value}`} value={`m${value}`} >{i}</option>);
             }
         }
 
@@ -48,19 +48,19 @@ export default function Form(props: Props) {
             <div className="formGroup" key="carTypes">
                 <label>select Vehicle type</label>
                 <select required name="carTypes" value={props.carTypes} onChange={(e) => props.handleChange(e)}>
-                    <option disabled value=""></option>
-                    <option value="4000101002">Personbiler</option>
-                    <option value="4000102001">Lastbiler </option>
-                    <option value="4000103001">Campingvogne  </option>
-                    <option value="4000104001">Busser</option>
-                    <option value="4000106001">Motorcykler</option>
+                    <option key="carTypesOp" disabled value=""></option>
+                    <option key="4000101002" value="4000101002">Personbiler</option>
+                    <option key="4000102001" value="4000102001">Lastbiler </option>
+                    <option key="4000103001" value="4000103001">Campingvogne  </option>
+                    <option key="4000104001" value="4000104001">Busser</option>
+                    <option key="4000106001" value="4000106001">Motorcykler</option>
                 </select>
             </div>
 
             <div className="formGroup" key="year">
                 <label>select year</label>
                 <select required name="year" value={props.year} onChange={e => props.handleChange(e)} >
-                    <option disabled value=""></option>
+                    <option disabled key="yearOp" value=""></option>
                     {yearElement}
                 </select>
             </div>
@@ -68,7 +68,7 @@ export default function Form(props: Props) {
             <div className="formGroup" key="month">
                 <label>select month</label>
                 <select required name="month" value={props.month} onChange={e => props.handleChange(e)} >
-                    <option disabled value=""></option>
+                    <option disabled key="monthOp" value=""></option>
                     {monthElement}
                 </select>
             </div>
